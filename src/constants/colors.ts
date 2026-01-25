fa6c7b8875aa76a452f8bc9880bd68a734e1cbf2
nofugazidev <oyebisimubarak4@gmail.com>
2026-01-25 23:26:53 +0100

    repo setup

---
+++ src/constants/colors.ts
@@ -0,0 +1,10 @@
+export const colors = {
+  primary: "#748c70",
+  secondary: "#5a6d57",
+  tertiary: "#d1d9cf",
+  lightGray: "#868686",
+  deepGray: "#404040",
+  black: "#0c0c0c",
+  white: "#ffffff",
+  offWhite: "#f0f2ef",
+} as const;
