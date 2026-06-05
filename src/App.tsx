8944e26f52d33d77ebcc29b76bef7068db224537
nofugazidev <oyebisimubarak4@gmail.com>
2026-06-05 06:06:50 +0100

    project restructuring

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,8 @@
-import modiWeek from "@/assets/images/week-one.png";
-import ModiWeek from "./components/modi-week";
-import { useState } from "react";
 
 export default function App() {
-  const [liked, setLiked] = useState(false);
-
-  const handleLike = () => {
-    setLiked((prev) => !prev);
-  };
   return (
     <div>
-      <ModiWeek
-        image={modiWeek}
-        day="tuesday"
-        liked={liked}
-        toggleLike={handleLike}
-      />
+      <p>welcome page</p>
     </div>
   );
 }
---
+++ src/components/default/navbar.tsx
@@ -0,0 +1,13 @@
+import logo from "@/assets/icons/logo.png";
+
+const Navbar: React.FC = () => {
+  return (
+    <nav>
+      <div className="logo"></div>
+      <div className="nav-item"></div>
+      <div className="func"></div>
+    </nav>
+  )
+}
+
+export default Navbar
