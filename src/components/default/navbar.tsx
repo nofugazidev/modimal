9ea42453f3d3725ff9274b2b952c87d173547363
nofugazidev <oyebisimubarak4@gmail.com>
2026-09-01 21:37:01 +0100

    nav dropdown logic built

--- src/components/default/navbar.tsx
+++ src/components/default/navbar.tsx
@@ -1,14 +1,48 @@
-// import logo from "@/assets/icons/logo.png";
+import logo from "@/assets/icons/logo.png";
+import search from "@/assets/icons/search.svg";
+import user from "@/assets/icons/user.svg";
+import heart from "@/assets/icons/heart.svg";
+import bag from "@/assets/icons/bag.png";
+
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 const Navbar: React.FC = () => {
   return (
-    <nav>
-      <div className="logo"></div>
-      <div className="nav-item"></div>
-      <div className="func"></div>
-      <h1>navbar</h1>
-    </nav>
-  )
-}
+    <StyledNavbar>
+      {/* modimal logo */}
+      <StyledLogo>
+        <img src={logo} alt="modimal-logo" />
+      </StyledLogo>
 
-export default Navbar
+      {/* navlinks */}
+      <DropdownContainer>
+        <StyledLink to="/new-in">collection</StyledLink>
+        <DropdownBox>
+          <DropdownItem to="/">lorem</DropdownItem>
+          <DropdownItem to="/">lorem</DropdownItem>
+          <DropdownItem to="/">lorem</DropdownItem>
+          <DropdownItem to="/">lorem</DropdownItem>
+          <DropdownItem to="/">lorem</DropdownItem>
+        </DropdownBox>
+      </DropdownContainer>
+    </StyledNavbar>
+  );
+};
+
+export default Navbar;
+
+const StyledNavbar = styled.nav``;
+const StyledLogo = styled.div``;
+const StyledLink = styled(Link)``;
+
+const DropdownBox = styled.div`
+  display: none;
+`;
+const DropdownItem = styled(Link)``;
+
+const DropdownContainer = styled.div`
+  &:hover ${DropdownBox}{
+    display: block;
+  }
+`;
